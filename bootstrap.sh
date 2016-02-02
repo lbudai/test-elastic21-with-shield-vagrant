93a31ec58843a0a500141eb6f464a34a9da5abd2
Laszlo Budai <stentor.bgyk@gmail.com>
2016-02-02 19:28:15 +0100

    bootstrap.sh: fixes
    Signed-off-by: Laszlo Budai <stentor.bgyk@gmail.com>

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,13 +12,16 @@ dpkg -i elasticsearch-2.1.1.deb
 /usr/share/elasticsearch/bin/plugin install license
 /usr/share/elasticsearch/bin/plugin install shield
 
+cp /vagrant/certs/server/server.jks /etc/elasticsearch
+chown -c elasticsearch /etc/elasticsearch/server.jks
+
 echo "192.168.33.10 vagrant-es-server" >> /etc/hosts 
 echo "192.168.33.1 vagrant-es-client" >> /etc/hosts
 
 echo "network.host: vagrant-es-server" >> /etc/elasticsearch/elasticsearch.yml
 echo "cluster.name: es-syslog-ng" >> /etc/elasticsearch/elasticsearch.yml
 
-echo "shield.ssl.keystore.path: /vagrant/certs/server/server.jks" >> /etc/elasticsearch/elasticsearch.yml
+echo "shield.ssl.keystore.path: /etc/elasticsearch/server.jks" >> /etc/elasticsearch/elasticsearch.yml
 echo "shield.ssl.keystore.password:      qqq123" >> /etc/elasticsearch/elasticsearch.yml
 echo "shield.transport.ssl:              true" >> /etc/elasticsearch/elasticsearch.yml
 echo "shield.ssl.hostname.verification.resolve.name: false" >> /etc/elasticsearch/elasticsearch.yml
